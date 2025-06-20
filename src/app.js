e540771efd05a406ed1cccc891afb2c6f34a34ad
mozammil-2020 <mozammil.xp@gmail.com>
2025-06-20 20:57:18 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -19,4 +19,44 @@ app.use(express.static("public"));
 
 app.use (cookieParser());
 
+// routes import
+
+import userRouter from "./routes/user.routs.js"
+
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+
+import tweetRouter from "./routes/tweet.routes.js"
+
+import subscriptionRoutes from "./routes/subscription.routes.js"
+
+import likeRouter from "./routes/like.routes.js"
+
+import playlistRouter from "./routes/playlist.routes.js"
+
+import commentsRouter from "./routes/comments.routes.js"
+
+import dashboardRouter from "./routes/dashbord.routes.js"
+
+import videoRouter from "./routes/video.routes.js"
+
+// routes decleration
+
+app.use("/api/v1/healthcheck", healthcheckRouter)
+
+app.use("/api/v1/users", userRouter)
+
+app.use("/api/v1/tweets", tweetRouter)
+
+app.use("/api/v1/subscription", subscriptionRoutes)
+
+app.use("/api/v1/videos", videoRouter)
+
+app.use("/api/v1/comments", commentsRouter)
+
+app.use("/api/v1/like", likeRouter)
+
+app.use("/api/v1/playlist", playlistRouter)
+
+app.use("/api/v1/dashboard", dashboardRouter)
+
 export { app }
